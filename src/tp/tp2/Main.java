283b2a7d0d759cb858e729b0778375939591c25d
MalicknND <ndiayemalick062@gmail.com>
2026-04-24 11:03:28 +0200

    tp 2 exceptions

---
+++ src/tp/tp2/Main.java
@@ -0,0 +1,48 @@
+package tp.tp2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Validez un mot de passe en respectant les conditions suivantes :
+        // - Le mot de passe doit comporter au moins 8 caractères.
+        // - Il doit contenir des lettres majuscules et minuscules
+        // - il doit contenir au minimum deux chiffres
+        // - pour chaque condition non respectée, affichez un message d'erreur spécifique.
+        // - Gerer les exceptions
+
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            System.out.println("Entrez un mot de passe : ");
+            String password = scanner.nextLine();
+
+            try {
+                validatePassword(password);
+                System.out.println("Mot de passe valide !");
+            } catch (Exception e) {
+                System.out.println("Message d'erreur : " + e.getMessage());
+            }
+
+
+        }
+
+    }
+
+    public static void validatePassword (String password) throws Exception {
+        if (password.length() < 8) {
+            throw new Exception("Le mot de passe doit comporter au moins 8 caractères.");
+        }
+        if (!password.matches(".*[A-Z].*")) {
+            throw new Exception("Le mot de passe doit contenir au moins une lettre majuscule.");
+        }
+        if (!password.matches(".*[a-z].*")) {
+            throw new Exception("Le mot de passe doit contenir au moins une lettre minuscule.");
+        }
+        if (password.replaceAll("\\D", "").length() < 2) {
+            throw new Exception("Le mot de passe doit contenir au moins deux chiffres.");
+            }
+
+    }
+}
+
+
