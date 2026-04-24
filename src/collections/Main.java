e1f38303f59c94494df1649ef5448abbab1ea205
MalicknND <ndiayemalick062@gmail.com>
2026-04-24 20:07:32 +0200

    interface iterator

--- src/collections/Main.java
+++ src/collections/Main.java
@@ -1,4 +1,16 @@
 package collections;
 
+
 public class Main {
+    public static void main(String[] args) {
+        GenericList<Integer> list = new GenericList<>();
+       var iterator = list.iterator();
+       list.add(1);
+       list.add(2);
+       list.add(3);
+       //[1,2,3]
+     while(iterator.hasNext()){
+         System.out.println(iterator.next());
+     }
+    }
 }
