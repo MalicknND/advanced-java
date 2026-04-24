d086ddd374bdb4b96afda6986123bfb4ef6f52c2
MalicknND <ndiayemalick062@gmail.com>
2026-04-24 16:00:22 +0200

    tp

---
+++ src/tp/genericite/GenericSet.java
@@ -0,0 +1,59 @@
+package tp.genericite;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class GenericSet<T> implements IGenericSet<T> {
+
+    private int length;
+    private T[] items;
+    private int count;
+
+    public GenericSet(int length) {
+        this.length = length;
+        this.items = ( T[] ) new Object[length];
+    }
+
+    @Override
+    public void add(T item) {
+        for (var tmpItem : items){
+            if (Objects.equals(tmpItem, item)){
+                return;
+            }
+            if (count < length){
+                items[count] = item;
+            }
+        }
+    }
+
+    @Override
+    public void remove(T item) {
+
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean contains(T item) {
+        return false;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public void print() {
+        System.out.println(Arrays.toString(items));
+    }
+}
+
---
+++ src/tp/genericite/IGenericSet.java
@@ -0,0 +1,11 @@
+package tp.genericite;
+
+public interface IGenericSet<T> {
+    void add(T item);
+    void remove(T item);
+    T get(int index);
+    void clear();
+    boolean contains(T item);
+    int getSize();
+    void print();
+}
